3bc3e82890d97c549bdb09ffcc10f903c3b6da2c
19522144 <19522144@gm.uit.edu.vn>
2021-04-26 15:05:27 +0700

    Upload Main Screen

--- app/src/main/java/com/ntsua/thelife/MainActivity.java
+++ app/src/main/java/com/ntsua/thelife/MainActivity.java
@@ -2,13 +2,21 @@ package com.ntsua.thelife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
